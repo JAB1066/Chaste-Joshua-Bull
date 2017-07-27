83083fa87eef61a279ef24cb1f6a2eae5600cc63
Joshua Bull <joshua.bull@maths.ox.ac.uk>
2017-07-27 14:35:32 +0100

    Parallel executable added for use on multicore machines

---
+++ apps/Distribute_ParallelSpheroidGrowthVaryingOxygenThresholds.py
@@ -0,0 +1,82 @@
+import multiprocessing
+import os
+import subprocess
+import itertools
+import time
+
+import numpy as np
+
+executable = '/scratch/Bull/chaste-release/projects/JoshuaBull/apps/Exe_ParallelDiffusionImplementationTesting'
+
+chaste_test_dir = os.environ.get('CHASTE_TEST_OUTPUT')
+path_to_output = os.path.join(chaste_test_dir, 'ParameterSweeps','SpheroidGrowthVaryingOxygenThreshold')
+
+if not(os.path.isfile(executable)):
+    raise Exception('Could not find executable: ' + executable)
+
+command_line_args = [' --ID ', ' --OC ', ' --G1 ', ' --HC ', ' --QC ', ' --CHD ']
+params_list = ['simulation_id', 'oxygenConsumptionRate', 'G1Duration', 'hypoxicConcentration', 'quiescentConcentration', 'criticalHypoxicDuration']
+
+today = time.strftime('%Y-%m-%dT%H%M')
+
+# Param ranges (in lists, for itertools product)
+oc = np.linspace(0.03, 0.09, num=3)
+g1 = np.linspace(4.0, 12.0, num=3)
+hc = np.linspace(0.10, 0.30, num=5)
+qc = np.linspace(0.30, 0.5, num=5)
+chd = np.linspace(4.0, 12.0, num=3)
+
+combined_iterable = enumerate(itertools.product(oc,g1,hc,qc,chd))
+
+
+def main():
+    run_simulations()
+
+
+# Create a list of commands and pass them to separate processes
+def run_simulations():
+
+    # Make a list of calls to a Chaste executable
+    command_list = []
+    
+    if not os.path.exists(path_to_output):
+    	os.makedirs(path_to_output)
+    
+    params_file = open(path_to_output + '/params_file.csv', 'w')
+    params_file.write(','.join(params_list) + '\n')
+
+    base_command = 'nice ' + executable
+
+    for idx, param_set in combined_iterable:
+    
+    	params_file.write(str(idx) + ',' + ",".join(map(str, param_set)) + '\n')
+
+        command = base_command 
+        command += ' --ID ' + str(idx)
+        
+        for arg in range(len(param_set)):
+        	command += command_line_args[arg+1] + str(param_set[arg])
+
+        command_list.append(command)
+        
+    params_file.close()
+
+    # Use processes equal to the number of cpus available
+    count = multiprocessing.cpu_count()
+
+    print("Py: Starting simulations with " + str(count) + " processes")
+
+    # Generate a pool of workers
+    pool = multiprocessing.Pool(processes=count)
+
+    # Pass the list of bash commands to the pool
+    # Wait at most one week
+    pool.map_async(execute_command, command_list).get(604800)
+
+
+# This is a helper function for run_simulation that runs bash commands in separate processes
+def execute_command(cmd):
+    return subprocess.call(cmd, shell=True)
+
+if __name__ == "__main__":
+    main()
---
+++ apps/src/Exe_ParallelSpheroidGrowthVaryingOxygenThresholds.cpp
@@ -0,0 +1,285 @@
+/*
+
+Copyright (c) 2005-2016, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
+#include <cxxtest/TestSuite.h>
+#include "CellBasedSimulationArchiver.hpp"
+#include "AbstractCellBasedTestSuite.hpp"
+
+#include "CellsGenerator.hpp"
+#include "TransitCellProliferativeType.hpp"
+#include "DifferentiatedCellProliferativeType.hpp"
+#include "UniformCellCycleModel.hpp"
+#include "HoneycombMeshGenerator.hpp"
+#include "GeneralisedLinearSpringForce.hpp"
+#include "ExecutableSupport.hpp"
+#include "OffLatticeSimulation.hpp"
+#include "SmartPointers.hpp"
+#include "NodesOnlyMesh.hpp"
+#include "NodeBasedCellPopulation.hpp"
+#include "NodeLocationWriter.hpp"
+
+#include "SimpleOxygenBasedCellCycleModelFreezeWhenHypoxic.hpp"
+#include "NoCellCycleModel.hpp"
+#include "WildTypeCellMutationState.hpp"
+#include "StemCellProliferativeType.hpp"
+#include "CellwiseSourceEllipticPde.hpp"
+#include "ConstBoundaryCondition.hpp"
+#include "RandomNumberGenerator.hpp"
+
+#include "AveragedSourceEllipticPde.hpp"
+#include "UniformSourceEllipticPde.hpp"
+#include "EllipticBoxDomainPdeModifier.hpp"
+#include "AbstractBoxDomainPdeModifier.hpp"
+#include "ApoptoticCellKiller.hpp"
+
+#include "CellMutationStatesCountWriter.hpp"
+#include "CellProliferativeTypesCountWriter.hpp"
+#include "CellProliferativePhasesCountWriter.hpp"
+
+#include "CellProliferativePhasesWriter.hpp"
+#include "CellProliferativeTypesWriter.hpp"
+#include "CellMutationStatesWriter.hpp"
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/lexical_cast.hpp>
+
+
+
+/*
+ * Prototype functions
+ */
+void SetupSingletons();
+void DestroySingletons();
+void SetupAndRunSimulation(std::string id_string, double oxygenConsumptionRate, double G1Duration, double hypoxicConcentration, double quiescentConcentration, double criticalHypoxicDuration);
+void OutputToConsole(std::string id_string, std::string leading);
+
+int main(int argc, char *argv[])
+{
+	// This sets up PETSc and prints out copyright information, etc.
+	//ExecutableSupport::StandardStartup(&argc, &argv);
+	ExecutableSupport::StartupWithoutShowingCopyright(&argc, &argv);
+
+	// Define command line options
+	boost::program_options::options_description general_options("This is a Chaste executable.\n");
+	general_options.add_options()
+                    						("help", "produce help message")
+											("ID", boost::program_options::value<std::string>(),"ID string for the simulation")
+											("OC", boost::program_options::value<double>()->default_value(0.03),"oxygenConsumptionRate: Rate at which cells consumpe oxygen")
+											("G1", boost::program_options::value<double>()->default_value(8.0),"G1Duration: Length of time cell spends in G1 state")
+											("HC", boost::program_options::value<double>()->default_value(0.15),"hypoxicConcentration: O2 conc at which cell becomes hypoxic")
+											("QC", boost::program_options::value<double>()->default_value(0.35),"quiescentConcentration: O2 conc at which cell becomes quiescent")
+											("CHD", boost::program_options::value<double>()->default_value(8.0),"criticalHypoxicDuration: duration cell must be hypoxic for before death");
+
+
+	// Define parse command line into variables_map
+	boost::program_options::variables_map variables_map;
+	boost::program_options::store(parse_command_line(argc, argv, general_options), variables_map);
+
+	// Print help message if wanted
+	if (variables_map.count("help"))
+	{
+		std::cout << setprecision(3) << general_options << "\n";
+		std::cout << general_options << "\n";
+		return 1;
+	}
+
+	// Get ID and name from command line
+	std::string id_string = variables_map["ID"].as<std::string>();
+	double oxygenConsumptionRate = variables_map["OC"].as<double>();
+	double G1Duration = variables_map["G1"].as<double>();
+	double hypoxicConcentration = variables_map["HC"].as<double>();
+	double quiescentConcentration = variables_map["QC"].as<double>();
+	double criticalHypoxicDuration = variables_map["CHD"].as<double>();
+
+	OutputToConsole(id_string, "Started");
+
+	SetupSingletons();
+	SetupAndRunSimulation(id_string,oxygenConsumptionRate,G1Duration,hypoxicConcentration,quiescentConcentration,criticalHypoxicDuration);
+	DestroySingletons();
+
+	OutputToConsole(id_string, "Completed");
+}
+
+void SetupSingletons()
+{
+	// Set up what the test suite would do
+	SimulationTime::Instance()->SetStartTime(0.0);
+
+	// Reseed with 0 for same random numbers each time, or time(NULL) or simulation_id to change each realisation
+	RandomNumberGenerator::Instance()->Reseed(time(NULL));
+	CellPropertyRegistry::Instance()->Clear();
+	CellId::ResetMaxCellId();
+}
+
+void DestroySingletons()
+{
+	// This is from the tearDown method of the test suite
+	SimulationTime::Destroy();
+	RandomNumberGenerator::Destroy();
+	CellPropertyRegistry::Instance()->Clear();
+}
+
+void OutputToConsole(std::string id_string, std::string leading)
+{
+	// Compose the message
+	std::stringstream message;
+	message << leading << " simulation with ID string " << id_string << std::endl;
+
+	// Send it to the console
+	std::cout << message.str() << std::flush;
+}
+
+void SetupAndRunSimulation(std::string id_string, double oxygenConsumptionRate, double G1Duration, double hypoxicConcentration, double quiescentConcentration, double criticalHypoxicDuration)
+{
+	{
+		// In previous tests, hypoxic cells continue their cell cycle until they reach the G1 phase. Here, we modify the cell cycle to cause them to freeze wherever they are in their cell cycle.
+		/** The next line is needed because we cannot currently run node based simulations in parallel. */
+		//EXIT_IF_PARALLEL;
+
+		// Generate Mesh:
+		// Make Vector
+		std::vector<Node<3>*> nodes;
+		// Add some nodes
+
+		double cubeDomainDistanceToBoundary = 30;
+
+		// Initialise with reasonably large spheroid
+		unsigned nodeNum=0;
+		double initialRadius = 10.0;
+		for (double x=-initialRadius; x<initialRadius; x++)
+		{
+			for (double y=-initialRadius; y<initialRadius; y++)
+			{
+				for (double z=-initialRadius; z<initialRadius; z++)
+				{
+					if(pow(x,2) + pow(y,2) + pow(z,2) < pow(initialRadius,2))
+					{
+						nodes.push_back(new Node<3>(nodeNum,  false,  x, y, z));
+						nodeNum++;
+					}
+				}
+			}
+		}
+
+		NodesOnlyMesh<3> mesh;
+		// Cut off length: 1.5 cell radii
+		mesh.ConstructNodesWithoutMesh(nodes, 1.5);
+
+		// Make cell pointers
+		std::vector<CellPtr> cells;
+		MAKE_PTR(WildTypeCellMutationState, p_state);
+		MAKE_PTR(StemCellProliferativeType, p_stem_type);
+
+		// Create tumour cells manually
+		for (unsigned i=0; i<firstMacrophageNode; i++)
+		{
+			SimpleOxygenBasedCellCycleModelFreezeWhenHypoxic* p_model = new SimpleOxygenBasedCellCycleModelFreezeWhenHypoxic;
+			p_model->SetDimension(3);
+
+			CellPtr p_cell(new Cell(p_state, p_model));
+			p_cell->SetCellProliferativeType(p_stem_type);
+			p_cell->GetCellData()->SetItem("oxygen", 1.0);
+
+			p_model->SetStemCellG1Duration(G1Duration);
+			p_model->SetTransitCellG1Duration(G1Duration);
+
+			p_model->SetHypoxicConcentration(hypoxicConcentration);
+			p_model->SetQuiescentConcentration(quiescentConcentration);
+			p_model->SetCriticalHypoxicDuration(criticalHypoxicDuration);
+
+			double birth_time = - RandomNumberGenerator::Instance()->ranf() *
+					(  p_model->GetStemCellG1Duration()
+							+ p_model->GetSG2MDuration() );
+			p_cell->SetBirthTime(birth_time);
+			cells.push_back(p_cell);
+		}
+
+		// Make cell population (3D)
+		NodeBasedCellPopulation<3> cell_population(mesh, cells);
+		cell_population.AddPopulationWriter<NodeLocationWriter>();
+		// Write summary statistic files
+		cell_population.AddCellPopulationCountWriter<CellProliferativeTypesCountWriter>();
+		cell_population.AddCellWriter<CellProliferativeTypesWriter>();
+
+		// Make PDE (Oxygen)
+		MAKE_PTR_ARGS(AveragedSourceEllipticPde<3>, p_pde, (cell_population, -oxygenConsumptionRate));//was -0.03
+		MAKE_PTR_ARGS(ConstBoundaryCondition<3>, p_bc, (1.0));
+		bool is_neumann_bc = false; // Dirichlet BCs
+
+		// Create a ChasteCuboid on which to base the finite element mesh used to solve the PDE
+		c_vector<double,3> centroid = cell_population.GetCentroidOfCellPopulation();
+		ChastePoint<3> lower(-cubeDomainDistanceToBoundary, -cubeDomainDistanceToBoundary, -cubeDomainDistanceToBoundary);
+		ChastePoint<3> upper(cubeDomainDistanceToBoundary, cubeDomainDistanceToBoundary, cubeDomainDistanceToBoundary);
+		MAKE_PTR_ARGS(ChasteCuboid<3>, p_cuboid, (lower, upper));
+
+		// Create a PDE modifier and set the name of the dependent variable in the PDE
+		MAKE_PTR_ARGS(EllipticBoxDomainPdeModifier<3>, p_pde_modifier, (p_pde, p_bc, is_neumann_bc, p_cuboid, 2.0));
+		p_pde_modifier->SetDependentVariableName("oxygen");
+		p_pde_modifier->SetBcsOnBoxBoundary(true); //was false
+
+		/* We then pass in the cell population into an {{{OffLatticeSimulation}}},
+		 * (this time with dimension 3) and set the output directory, output multiple and end time. */
+		OffLatticeSimulation<3> simulator(cell_population);
+		simulator.AddSimulationModifier(p_pde_modifier);
+		std::stringstream output_directory;
+		output_directory << "ParameterSweeps/SpheroidGrowthVaryingOxygenThreshold/" << id_string;
+		simulator.SetOutputDirectory(output_directory.str());
+		simulator.SetSamplingTimestepMultiple(60); // One visualisation every 30 minutes...
+		simulator.SetEndTime(96.0); // ...for 96 hours
+
+		/* Again we create a force law (this time with dimension 3), and pass it to the {{{OffLatticeSimulation}}}.*/
+		MAKE_PTR(GeneralisedLinearSpringForce<3>, p_force);
+		p_force->SetMeinekeSpringStiffness(30.0);
+		p_force->SetCutOffLength(1.5);
+		simulator.AddForce(p_force);
+
+		// Killer which removes apoptotic cells
+		MAKE_PTR_ARGS(ApoptoticCellKiller<3>, p_apoptosis_killer, (&cell_population));
+		simulator.AddCellKiller(p_apoptosis_killer);
+
+		/* To run the simulation, we call {{{Solve()}}}. */
+		simulator.Solve();
+
+
+		/* To avoid memory leaks, we conclude by deleting any pointers that we created in the test.*/
+		for (unsigned i=0; i<nodes.size(); i++)
+		{
+			delete nodes[i];
+		}
+	}
+
+}
